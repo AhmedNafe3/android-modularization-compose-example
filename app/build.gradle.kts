bdc606c1c8c70287fb92a321f2d3376e723ea7f9
Ahmed Nafea <codenafe3@gmail.com>
2023-12-11 10:50:41 +0300

    upgrade to android version 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.jiftun.android_modularization_compose_example"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.jiftun.android_modularization_compose_example"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
